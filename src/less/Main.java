65511b2f6d9924865cd3e58f85bf7d1931a3c53a
Gitch14 <vladbuckach@ukr.net>
2022-04-01 19:35:59 +0300

    почти конец

--- src/less/Main.java
+++ src/less/Main.java
@@ -1,6 +1,11 @@
 package less;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
 
@@ -97,15 +102,15 @@ class VoucherInfo {
     static class Voucher {
         public int id_Voucher;
         public String Name;
-        public String date_start;
-        public String date_end;
+        public Date date_start;
+        public Date date_end;
         public int id_City_voucher;
         public String services;
         public int price;
         public int count;
 
 
-        public Voucher(int id_Voucher, String name, String date_start, String date_end, int id_City_voucher, String services, int price, int count) {
+        public Voucher(int id_Voucher, String name, Date date_start, Date date_end, int id_City_voucher, String services, int price, int count) {
             this.id_Voucher = id_Voucher;
             Name = name;
             this.date_start = date_start;
@@ -134,7 +139,7 @@ class VoucherInfo {
 
     static ArrayList<Object> voucherList = new ArrayList<>();
 
-    public void AddVoucher(int id_Voucher, String name, String date_start, String date_end, int id_City_voucher, String services, int price, int count) {
+    public void AddVoucher(int id_Voucher, String name, Date date_start, Date date_end, int id_City_voucher, String services, int price, int count) {
         voucherList.add(new Voucher(id_Voucher, name,date_start, date_end, id_City_voucher, services, price, count));
     }
 }// Voucher class end
@@ -150,10 +155,10 @@ class SaleInfo {
     static class Sale {
         public int id_Sale;
         public String Name;
-        public int date_Sale;
+        public Date date_Sale;
 
 
-        public Sale(int id_Sale, String name, int date_Sale) {
+        public Sale(int id_Sale, String name, Date date_Sale) {
             this.id_Sale = id_Sale;
             Name = name;
             this.date_Sale = date_Sale;
@@ -172,7 +177,7 @@ class SaleInfo {
 
     static ArrayList<Object> saleList = new ArrayList<>();
 
-    public static void AddSale(int id_Sale, String name, int date_Sale) {
+    public static void AddSale(int id_Sale, String name, Date date_Sale) {
         saleList.add(new Sale(id_Sale, name, date_Sale));
     }
 }
@@ -180,93 +185,40 @@ class SaleInfo {
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        DateFormat format = new SimpleDateFormat("d MMMM yyyy", new Locale("ru"));
         Scanner scanner = new Scanner(System.in);
         int key_Client = 0, id_City = 0, id_Voucher = 0, id_Sale = 0;
         boolean b = true;
 
         do {
+            
             key_Client++;
-            System.out.println("Проверка");
-            String verf = scanner.nextLine();
-            System.out.println("Введите имя клиента");
-            String name = scanner.nextLine();
-            System.out.println("Введите мобильный клиента");
-            String phone_number = scanner.nextLine();
-            System.out.println("Введите номер паспорта клиента");
-            String passport_ID = scanner.nextLine();
-            System.out.println("Введите скидку для клиента");
-            int skidka = scanner.nextInt();
-
-            System.out.println("Введите 1 для продолжения");
-            int checknum = scanner.nextInt();
-            if (checknum == 1) {
-                b = true;
-            } else {
-                b = false;
-            }
-            Client.AddList(new Client.User(key_Client,name,phone_number,passport_ID,skidka));
-        } while (b == true);
+            System.out.println("Введите имя клиента, номер телефона, номер паспорта и скидку разделяйте их двумя пробелами");
+            String prodStr1 = scanner.nextLine();
+            String[] client = prodStr1.split("  ");
+            Client.AddList(new Client.User(key_Client,client[0], client[1], client[2], Integer.parseInt(client[3].trim())));
 
 
-        do {
             id_City++;
-            System.out.println("Проверка");
-            String verf = scanner.nextLine();
-            System.out.println("Введите название путевки");
-            String name_place = scanner.nextLine();
-            System.out.println("Введите страну");
-            String country = scanner.nextLine();
+            System.out.println("Введите название города, и страну разделяйте их двумя пробелами");
+            String prodStr2 = scanner.nextLine();
+            String[] city = prodStr2.split("  ");
+            CityInfo.AddList(new CityInfo.City(id_City, city[0], city[1]));
 
 
-            System.out.println("Введите 1 для продолжения");
-            int checknum = scanner.nextInt();
-            if (checknum == 1) {
-                b = true;
-            } else {
-                b = false;
-            }
-            CityInfo.AddList(new CityInfo.City(id_City,name_place,country));
-        } while (b == true);
-
-        do {
             id_Voucher++;
-            System.out.println("Проверка");
-            String verf = scanner.nextLine();
-            System.out.println("Введите название путевки");
-            String name = scanner.nextLine();
-            System.out.println("Введите начало поездки");
-            String date_start = scanner.nextLine();
-            System.out.println("Введите конец поездки");
-            String date_end = scanner.nextLine();
-            System.out.println("Введите город(key int)"); // ключ
-            int id_City_voucher = scanner.nextInt();
-            System.out.println("Введите услугу");
-            String services = scanner.nextLine();
-            System.out.println("Введите цену на услугу");
-            int price = scanner.nextInt();
-            System.out.println("Введите количество людей");
-            int count = scanner.nextInt();
-
-            System.out.println("Введите 1 для продолжения");
-            int checknum = scanner.nextInt();
-            if (checknum == 1) {
-                b = true;
-            } else {
-                b = false;
-            }
-            VoucherInfo.AddVoucher(new VoucherInfo.Voucher(id_Voucher, name,date_start, date_end, id_City_voucher, services, price, count));
-        } while (b == true);
+            System.out.println("Введите название путевки, дата начало, дата конца, город ключ, услуги, ценна и количество человек скидку разделяйте их двумя пробелами");
+            String prodStr3 = scanner.nextLine();
+            String[] voucher = prodStr3.split("  ");
+            VoucherInfo.AddVoucher(new VoucherInfo.Voucher(id_Voucher,voucher[0], format.parse(voucher[1]), format.parse(voucher[2]), Integer.parseInt(voucher[3].trim()), voucher[4], Integer.parseInt(voucher[5].trim()),Integer.parseInt(voucher[6].trim())));
 
 
-        do {
+            System.out.println("Введите ФИО сотрудника и дата продажы разделяйте их двумя пробелами");
             id_Sale++;
-            System.out.println("Проверка");
-            String verf = scanner.nextLine();
-            System.out.println("Введите название путевки");
-            String name = scanner.nextLine();
-            System.out.println("Дата продажи");
-            int date_Sale = scanner.nextInt();
+            String prodStr4 = scanner.nextLine();
+            String[] sale = prodStr4.split("  ");
+            SaleInfo.AddList(new SaleInfo.Sale(id_City, sale[0], format.parse(sale[1])));
 
 
             System.out.println("Введите 1 для продолжения");
@@ -276,7 +228,6 @@ public class Main {
             } else {
                 b = false;
             }
-            SaleInfo.AddList(new SaleInfo.Sale(id_Sale, name,date_Sale));
         } while (b == true);
 
 
@@ -295,11 +246,5 @@ public class Main {
         for(Object s4 : SaleInfo.saleList) {
             System.out.println(s4);
         }
-
-
-
-
-
     }
-
 }
